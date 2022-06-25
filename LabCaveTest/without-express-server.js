e1098220b5884508c8354cc761cecc168b2c4880
MAGMa <36223494+miguelangelgil@users.noreply.github.com>
2022-06-25 23:57:57 +0200

    implemented server without express

---
+++ LabCaveTest/without-express-server.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const moment = require('moment');
+
+/**
+ * Create the server
+ */
+const app = http.createServer((req, res)=>{
+    //Return the currentTime of the server
+    if(req.url.indexOf("/time")!== -1)
+    {
+        res.writeHead(200,{'Content-type': 'text/datetime'});
+        res.write(moment().format(req.url.substring(req.url.indexOf("=") + 1, req.url.length-1)));
+        res.end();
+    }else
+    {
+        res.writeHead(404,{'Content-type': 'text/plain'});
+        res.write('Service not found');
+        res.end();
+    }
+   
+   
+}); 
+
+/**
+ * The port listening by the server 
+ */
+app.listen(3000, ()=>{
+    console.log('Server listening on port 3000');
+});
